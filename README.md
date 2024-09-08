cfaacccb841bee3603c93fab0a47ebd9438e3a5f
Ashishkum112 <ashishkumarr0214@gmail.com>
2024-09-08 19:54:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+3D Portfolio Website
+Check out my live portfolio built with React, HTML, CSS, and JavaScript.
+
+Live Link - 
+https://3d-portfolio-website-virid.vercel.app/
+
+Features
+
+-Interactive 3D elements
+-Responsive design
+-Modern UI/UX
+
+Tech Stack
+
+-React
+-HTML
+-CSS
+-JavaScript
---
+++ src/constants/index.js
@@ -0,0 +1,213 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    quickhire,
+    snake,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Frontend Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Digital Marketing",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Web Development Intern",
+      company_name: "OctaNet Services Pvt Ltd",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "June 2024 - July 2024",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Open Source Contributor",
+      company_name: "GirlScript Summer of Code",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "May 2024 - June 2024",
+      points: [
+        "I have contributed to various open-source projects and learned from industry experts.",
+        "These contributions helped me gain hands-on experience and hone my skills.",
+        "This combination has significantly enriched my knowledge and professional growth.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "QuickHire",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "React.js",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Node.js",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Express.js",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "MongoDB",
+          color: "green-text-gradient",
+        },
+
+
+      ],
+      image: quickhire,
+      source_code_link: "https://job-portal-7iyl.onrender.com/",
+    },
+    {
+      name: "Snake Xenzia ",
+      description:
+        "A Traditional game which has a snake where it stores the highest user with funny sounds",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "JAVASCRIPT",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: snake,
+      source_code_link: "https://github.com/Ashishkum112/Snake-Game",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
